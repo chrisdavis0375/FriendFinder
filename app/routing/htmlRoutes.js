8406ea7fb309d407ed5cb3afd4d04f160c37e463
Chris Davis <chrisdavis0375@gmail.com>
2019-05-13 17:26:02 -0700

    Routes logic fixed and survey selectors fixed

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,7 +6,7 @@ var path = require("path");
 module.exports = function(app) {
 
 
-    app.get("/survey", function(res, req) {
+    app.get("/survey", function(req, res) {
         console.log(res);
         res.sendFile(path.join(__dirname, "../public/survey.html"))
     }) 
